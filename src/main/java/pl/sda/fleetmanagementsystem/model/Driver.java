d8c9f8849978eea25ae2f2ade7754afe0ed1cf48
Mariusz Kowalczuk <lordmer@gmail.com>
2019-06-15 11:20:15 +0200

    create #3

---
+++ src/main/java/pl/sda/fleetmanagementsystem/model/Driver.java
@@ -0,0 +1,8 @@
+package pl.sda.fleetmanagementsystem.model;
+
+/**
+ * @author Mariusz Kowalczuk
+ */
+public class Driver extends User {
+
+}
--- src/main/java/pl/sda/fleetmanagementsystem/model/User.java
+++ src/main/java/pl/sda/fleetmanagementsystem/model/User.java
@@ -1,4 +1,4 @@
-package pl.sda.model;
+package pl.sda.fleetmanagementsystem.model;
 
 import lombok.Getter;
 import lombok.Setter;
