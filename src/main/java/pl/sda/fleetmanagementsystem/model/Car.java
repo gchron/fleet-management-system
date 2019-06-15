01d95e1a3adcfd9e83bc6ba9a16e3cab417e1964
cronix <grzegorz.chron@gmail.com>
2019-06-15 11:02:46 +0200

    added Car entity. Closes #2

---
+++ src/main/java/pl/sda/fleetmanagementsystem/model/Car.java
@@ -0,0 +1,21 @@
+package pl.sda.fleetmanagementsystem.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String brand;
+    private String model;
+    private String productionYear;
+    private String mileage;
+    private Double engineCapacity;
+    private Driver driver;
+    private TechnicalInspection technicalInspection;
+}
