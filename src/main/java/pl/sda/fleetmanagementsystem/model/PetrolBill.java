0e587801449cd0aff58190f23c94221324de50af
Mariusz Kowalczuk <lordmer@gmail.com>
2019-06-15 11:55:23 +0200

    closes #7

---
+++ src/main/java/pl/sda/fleetmanagementsystem/model/PetrolBill.java
@@ -0,0 +1,23 @@
+package pl.sda.fleetmanagementsystem.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+/**
+ * @author Mariusz Kowalczuk
+ */
+@Entity
+public class PetrolBill {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private BigDecimal value;
+    private LocalDate date;
+
+
+}
