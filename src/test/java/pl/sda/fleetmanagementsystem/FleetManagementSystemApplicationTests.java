ff0803aa35e8e2bac2ee0ff0a48f2e8a72ac4b71
Mariusz Kowalczuk <lordmer@gmail.com>
2019-06-16 13:11:52 +0200

    properties

--- src/test/java/pl/sda/fleetmanagementsystem/FleetManagementSystemApplicationTests.java
+++ src/test/java/pl/sda/fleetmanagementsystem/FleetManagementSystemApplicationTests.java
@@ -2,9 +2,14 @@ package pl.sda.fleetmanagementsystem;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@SpringBootApplication
+@PropertySource(value = {"application.properties", "application-ext.properties"},
+        ignoreResourceNotFound = true)
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class FleetManagementSystemApplicationTests {
