8125b6e36bc89a97ba4b44092fe25f9c0f2e5bf9
Mariusz Kowalczuk <lordmer@gmail.com>
2019-06-19 23:11:58 +0200

    Merge branch 'develop' of C:\Users\lordm\Development\fleet-management-system with conflicts.

--- src/main/java/pl/sda/fleetmanagementsystem/controllers/DriverController.java
+++ src/main/java/pl/sda/fleetmanagementsystem/controllers/DriverController.java
@@ -2,12 +2,10 @@ package pl.sda.fleetmanagementsystem.controllers;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import pl.sda.fleetmanagementsystem.dto.DriverDto;
+import pl.sda.fleetmanagementsystem.dto.DriverLicenseAssignmentDto;
 import pl.sda.fleetmanagementsystem.dto.DrivingLicenseDto;
 import pl.sda.fleetmanagementsystem.service.DriverFinder;
 import pl.sda.fleetmanagementsystem.service.DriverService;
@@ -45,4 +43,30 @@ public class DriverController {
         driverService.create(driver);
         return "redirect:/drivers";
     }
+
+    @GetMapping("/main/{id}")
+    ModelAndView showUser(@PathVariable Integer id) {
+        ModelAndView modelAndView = new ModelAndView("drivers/main.html");
+        modelAndView.addObject("driver", driverFinder.findById(id));
+        return modelAndView;
+
+    }
+
+    @GetMapping("/setDrivingLicense")
+    ModelAndView setDrivingLicense(@RequestParam String id) {
+
+        ModelAndView modelAndView = new ModelAndView("drivers/setDrivingLicense");
+        modelAndView.addObject("driverId", id);
+        modelAndView.addObject("assignment", new DriverLicenseAssignmentDto());
+        return modelAndView;
+    }
+
+    @PostMapping("setDrivingLicense")
+    String setDrivingLicense(@ModelAttribute DriverLicenseAssignmentDto assignment) {
+
+        driverService.setDrivingLicense(Integer.valueOf(assignment.getDriverId()), assignment);
+        return "redirect:/drivers";
+
+    }
+
 }
